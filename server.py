855dfdb90a6886fac318d3e2fcbbc3a23e86da28
krishnashrimali88 <krishnashrimali008@gmail.com>
2026-06-05 14:05:55 +0530

    few changes

--- server.py
+++ server.py
@@ -1,12 +1,26 @@
-from flask import Flask
-
+from flask import Flask,send_from_directory
+import os
+from pathlib import Path
 app = Flask(__name__)
 
+ath = os.path.dirname(__file__)
 
-@app.route('/rt')
-def test():
-    return 'hello'
+nath = os.path.join(ath,'served_folder')
+
+new = Path(nath)
+print(nath)
+
+
+@app.route('/')
+def list_files():
+    files = os.listdir(new)
+    return '<br>'.join(f'<a href="/files/{f}">{f}</a>' for f in files)
+
+@app.route('/files/<path:filename>')
+def test(filename):
+    return send_from_directory(new,filename)
 
 
 
-app.run(host = '127.0.0.1',port = 8080)
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1',port = 8080)
