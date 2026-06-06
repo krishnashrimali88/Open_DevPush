40f98beb10e8a645bc86ff76bafcc7fe7f52122f
krishnashrimali88 <krishnashrimali008@gmail.com>
2026-06-06 18:49:32 +0530

    progress

--- client_gui.py
+++ client_gui.py
@@ -1,24 +1,30 @@
-from customtkinter import CTk,CTkTextbox,CTkButton,filedialog
-from connection import update
+from customtkinter import CTk,CTkFrame,CTkButton,filedialog
+from connection import update,down
 import asyncio
 import threading
 
 
 class client_gui(CTk):
+  
     def __init__(self):
         super().__init__()
+        self.dir = None
         self.title('mygui')
         self.geometry('920x560')
         self.resizable(False,False)
 
-        self.text = CTkTextbox(self,width=250)
-        self.text.pack()
+        self.frame = CTkFrame(self,width=350,height=300)
+        self.frame.pack()
         
-        self.get = CTkButton(self,width=50,text='update',command=self.runner)
+
+        self.get = CTkButton(self,width=50,text='update',command=lambda:asyncio.run(update(self.frame)))
         self.get.pack()
 
-        self.get = CTkButton(self,width=50,text='choose directory',command=self.choose)
-        self.get.pack()
+        self.net = CTkButton(self,width=50,text='choose directory',command=self.choose)
+        self.net.pack()
+
+        self.download = CTkButton(self,width=50,text='download',command=lambda:asyncio.run(down(self.dir)))
+        self.download.pack()    
 
 
 
--- connection.py
+++ connection.py
@@ -1,10 +1,13 @@
 import aiohttp
 import os
 from pathlib import Path
+from customtkinter import CTkCheckBox
+
 
 base = Path(os.path.dirname(__file__))
 
-async def get():
+async def down(addr):
+    addr = Path(addr)
     async with aiohttp.ClientSession() as session:
     
         async with session.get("http://127.0.0.1:8080/") as answer:
@@ -16,21 +19,27 @@ async def get():
                 f"http://127.0.0.1:8080/files/{task}"
             ) as response:
 
-                filepath = base /'to_do'/ task
+                
+                new_path = addr / task
 
-                with open(filepath, "wb") as f:
+
+                with open(new_path, "wb") as f:
                     async for chunk in response.content.iter_chunked(8192):
                         f.write(chunk)
 
                 print(f"Downloaded: {task}")
 
-async def update(textbox):
+async def update(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
     async with aiohttp.ClientSession() as session:
         async with session.get("http://127.0.0.1:8080/") as answer:
             tasks = await answer.json()
     
     for t in tasks:
-        textbox.insert('end',f'\n{t}')
+        CTkCheckBox(frame,text=f'{t}').pack()
 
 
 
